e0d05cda0359ef269fafecf618fbad4769c1a42c
Ivan <ivan@getconvey.com>
2021-09-26 23:17:56 +0200

    IisSe lookup provider

--- src/main/java/rs/thedespot/controller/LookupController.java
+++ src/main/java/rs/thedespot/controller/LookupController.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import rs.thedespot.model.DnsInfo;
 import rs.thedespot.model.DomainStatus;
 import rs.thedespot.model.LookupResponse;
 import rs.thedespot.service.DnsService;
 import rs.thedespot.service.WhoIsException;
 import rs.thedespot.service.WhoIsService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,6 +34,8 @@ public class LookupController {
         LookupResponse response = whoIsService.resolveDomain(domainName);
         if (response.getDomainStatus() != DomainStatus.NotRegistered) {
             response.setDnsInfo(dnsService.resolveAddress(domainName));
+        } else {
+            response.setDnsInfo(new DnsInfo().setHostAddress(null).setNameServers(List.of()));
         }
 
         return ResponseEntity.ok(response);
--- src/main/java/rs/thedespot/lookup/IisSeProvider.java
+++ src/main/java/rs/thedespot/lookup/IisSeProvider.java
@@ -1,10 +1,52 @@
 package rs.thedespot.lookup;
 
+import rs.thedespot.model.DomainStatus;
+import rs.thedespot.model.LookupResponse;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.stream.Stream;
+
 public class IisSeProvider extends LookupProvider {
 
     @Override
     protected String getLookupURL() {
         return "whois.iis.se";
+    }
+
+    @Override
+    protected LookupResponse parseResponse(String response) {
+        if (response.contains("\" not found.")) {
+            return new LookupResponse()
+                    .setDomainStatus(DomainStatus.NotRegistered);
+        }
+
+        String[] lines = response.split("\\n");
+
+        String state = Stream.of(lines)
+                .filter(l -> l.startsWith("state:"))
+                .map(l -> l.replace("state:", "").replace(" ", ""))
+                .findFirst()
+                .get();
+
+        LocalDate createdDate = Stream.of(lines)
+                .filter(l -> l.startsWith("expires:"))
+                .map(l -> l.replace("expires:", "").replace(" ", ""))
+                .map(LocalDate::parse)
+                .findFirst()
+                .get();
+
+        LocalDate expirationDate = Stream.of(lines)
+                .filter(l -> l.startsWith("created:"))
+                .map(l -> l.replace("created:", "").replace(" ", ""))
+                .map(LocalDate::parse)
+                .findFirst()
+                .get();
+
+        return new LookupResponse()
+                .setDomainStatus(DomainStatus.Active)
+                .setRegistrationDate(createdDate.atStartOfDay(ZoneId.systemDefault()))
+                .setExpirationDate(expirationDate.atStartOfDay(ZoneId.systemDefault()));
     }
 
 }
