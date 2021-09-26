1f2d34542e5ea32b3f570465226cd68e345489ff
Ivan <ivan@getconvey.com>
2021-09-26 13:01:58 +0200

    Email refactoring

--- src/main/java/rs/thedespot/service/EmailService.java
+++ src/main/java/rs/thedespot/service/EmailService.java
@@ -4,10 +4,6 @@ import com.mailjet.client.MailjetClient;
 import com.mailjet.client.MailjetRequest;
 import com.mailjet.client.MailjetResponse;
 import com.mailjet.client.resource.Emailv31;
-import com.mailjet.client.transactional.SendContact;
-import com.mailjet.client.transactional.SendEmailsRequest;
-import com.mailjet.client.transactional.TransactionalEmail;
-import com.mailjet.client.transactional.response.SendEmailsResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.json.JSONArray;
@@ -22,22 +18,26 @@ public class EmailService {
 
     @SneakyThrows
     public void sendEmail(String emailTo, String domainName) {
-        TransactionalEmail message1 = TransactionalEmail
-                .builder()
-                .to(new SendContact(emailTo, "User"))
-                .from(new SendContact("ivan.nikolic.dev@gmail.com", "RNIDS"))
-                .htmlPart("Domen " + domainName + " koji ste zapamtili uskoro istice. Pozurite da ga rezervisete")
-                .subject("Domen " + domainName + " uskoro istice")
-//                .header("test-header-key", "test-value")
-                .build();
-
-        SendEmailsRequest request = SendEmailsRequest
-                .builder()
-                .message(message1)
-                .build();
-
-        SendEmailsResponse response = request.sendWith(mailjetClient);
-        System.out.println(response);
+        MailjetRequest request;
+        MailjetResponse response;
+        request = new MailjetRequest(Emailv31.resource)
+                .property(Emailv31.MESSAGES, new JSONArray()
+                        .put(new JSONObject()
+                                .put(Emailv31.Message.FROM, new JSONObject()
+                                        .put("Email", "ivan.nikolic.dev@gmail.com")
+                                        .put("Name", "RNIDS"))
+                                .put(Emailv31.Message.TO, new JSONArray()
+                                        .put(new JSONObject()
+                                                .put("Email", emailTo)
+                                                .put("Name", "User")))
+                                .put(Emailv31.Message.TEMPLATEID, 3201654)
+                                .put(Emailv31.Message.TEMPLATELANGUAGE, true)
+                                .put(Emailv31.Message.SUBJECT, "Domen {{var:domain_name}} je sada slobodan")
+                                .put(Emailv31.Message.VARIABLES, new JSONObject()
+                                        .put("domain_name", domainName))));
+        response = mailjetClient.post(request);
+        System.out.println(response.getStatus());
+        System.out.println(response.getData());
     }
 
 }
